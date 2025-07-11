b25c217671f228f5a35f41f6fb57f3fe9bd2c2e5
Jamtech Dev <jamtechba@gmail.com>
2025-07-11 10:56:06 +0530

    Fixed import issue

--- src/Pages/Register/index.jsx
+++ src/Pages/Register/index.jsx
@@ -3,7 +3,7 @@ import { Link, useNavigate } from "react-router";
 import api from "../../api.js";
 import Header_V1 from "../../Component/header/header-v1/header.jsx";
 import "./index.css";
-import PlacesAutocompleteInput from "../../Component/Autocomplete/PlacesAutoComplete.jsx";
+// import PlacesAutocompleteInput from "../../Component/Autocomplete/PlacesAutoComplete.jsx";
 
 export default function Register() {
   const navigate = useNavigate();
